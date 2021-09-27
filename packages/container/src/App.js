85a384b7c38309e58c3bfbe0ceee95bcfad96026
Stanislav Petrov <catnislav@gmail.com>
2021-09-27 14:36:06 +0300

    updated container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>
-        Hello my friend ! I'm container, you are inside me !
+        Hello ! I'm container, you are inside me !
       </h1>
       <hr />
       <MarketingApp />
