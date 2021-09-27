ab80b1df08541dec93582e620a554000e91a3aa2
Stanislav <catnislav@gmail.com>
2021-09-27 15:37:20 +0300

    Finished feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>
-        Hello world ! I'm container, you are inside me !
+        Hello world ! I'm container, you are inside me ! And I'm not crazy !
       </h1>
       <hr />
       <MarketingApp />
